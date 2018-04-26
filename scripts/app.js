c7263900c38adbd4ade1e107192f71c1b64f59d0
Nick Renteria <nrenteria303@gmail.com>
2018-04-26 12:27:26 -0600

    added mq's and hover func

---
+++ scripts/app.js
@@ -0,0 +1,15 @@
+// =========================================
+// Front Page ==============================
+// =========================================
+
+// Variables
+const $frontNavItem = $('.front-nav-item');
+const $frontPosDesc = $('.front-pos-desc');
+
+// Functions
+
+// $frontPosDesc.hide();
+
+// $frontNavItem.hover() {
+
+// }
