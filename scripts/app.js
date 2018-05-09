c5e4a6d8f55d01d20eb11105330f194a0854e37e
Nick Renteria <nrenteria303@gmail.com>
2018-05-09 00:00:46 -0600

    added mixins: gradient & 3dtransforms

--- scripts/app.js
+++ scripts/app.js
@@ -2,14 +2,4 @@
 // Front Page ==============================
 // =========================================
 
-// Variables
-const $frontNavItem = $('.front-nav-item');
-const $frontPosDesc = $('.front-pos-desc');
 
-// Functions
-
-// $frontPosDesc.hide();
-
-// $frontNavItem.hover(function() {
-//     $frontPosDesc.show();
-// } );
