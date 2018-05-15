890fb001d1169d519a1132f5b187c0d03154ba2b
Nick Renteria <Nick Renteria>
2018-05-15 02:35:05 -0600

    site overhaul

--- scripts/app.js
+++ scripts/app.js
@@ -2,4 +2,21 @@
 // Front Page ==============================
 // =========================================
 
+$(document).ready(function() {
+    adjustBody();
+    $(window).on('resize orientationchange', adjustBody);
 
+    function adjustBody() {
+        var headHeight = $('header').outerHeight();
+        $('.banner').css('padding-top', headHeight);
+    }
+});
+
+$(window).scroll(function() {
+    if ($(window).scrollTop() > 20) {
+        $('header').addClass('is-fixed');
+    }
+    else {
+        $('header').removeClass('is-fixed');
+    }
+});
