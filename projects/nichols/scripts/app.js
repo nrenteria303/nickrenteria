43ced52c25c96679bcea5319e0a5e2f83d19dffa
Nick Renteria <Nick Renteria>
2019-05-12 20:02:54 -0600

    nichols updates

--- projects/nichols/scripts/app.js
+++ projects/nichols/scripts/app.js
@@ -1,6 +1,8 @@
 const menuIcn = document.getElementById("menu_bars");
 const nav = document.getElementById('nav');
+const navExit = document.getElementById('menuExitLayer');
 const bannerHeading = document.getElementById('fancy_heading');
+const bannerShade = document.getElementById('banner_shade_top');
 const mainFront = document.getElementById('front_page_main');
 const banner = document.getElementById('banner');
 const bar1 = document.getElementById('bar_1');
@@ -9,32 +11,35 @@ const bar3 = document.getElementById('bar_3');
 var menuShowing = false;
 
 function toggleNav() {
-    menuShowing = !menuShowing;
     if (!menuShowing) {
         nav.style.left = 0;
+        navExit.style.left = '200px';
         bar1.classList.add('right_x');
         bar2.classList.add('left_x');
         bar3.classList.add('right_x');
     } else {
         nav.style.left = '-200px';
+        navExit.style.left = '-100vw';
         bar1.classList.remove('right_x');
         bar2.classList.remove('left_x');
         bar3.classList.remove('right_x');
-    }
+    }    
+    menuShowing = !menuShowing;
 }
 
-// function hideMenu() {
-//     menuShowing = false;
-//     nav.style.left = '-200px';
-//     bar1.classList.remove('right_x');
-//     bar2.classList.remove('left_x');
-//     bar3.classList.remove('right_x');
-// }
+function hideMenu() {
+    menuShowing = false;
+    nav.style.left = '-200px';
+    navExit.style.left = '-100vw';
+    bar1.classList.remove('right_x');
+    bar2.classList.remove('left_x');
+    bar3.classList.remove('right_x');
+}
 
 function showBannerHeading() {
+    bannerShade.style.top = 0;
     bannerHeading.style.opacity = 1;
 }
 
 menuIcn.addEventListener('click', toggleNav);
-// mainFront.addEventListener('click', hideMenu);
-// banner.addEventListener('click', hideMenu);
+navExit.addEventListener('click', hideMenu);
