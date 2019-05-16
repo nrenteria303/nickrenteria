902be7a584ba1105a7a33c4d41f0f54819c31b89
Nick Renteria <Nick Renteria>
2019-05-15 21:12:30 -0600

    nichols: slideshow, footer

--- projects/nichols/scripts/home.js
+++ projects/nichols/scripts/home.js
@@ -1,6 +1,10 @@
 const menuIcn = document.getElementById("menu_bars");
 const nav = document.getElementById('nav');
 const navExit = document.getElementById('menuExitLayer');
+const bannerHeading = document.getElementById('fancy_heading');
+const bannerShade = document.getElementById('banner_shade_top');
+const mainFront = document.getElementById('front_page_main');
+const banner = document.getElementById('banner');
 const bar1 = document.getElementById('bar_1');
 const bar2 = document.getElementById('bar_2');
 const bar3 = document.getElementById('bar_3');
@@ -30,6 +34,11 @@ function hideMenu() {
     bar1.classList.remove('right_x');
     bar2.classList.remove('left_x');
     bar3.classList.remove('right_x');
+}
+
+function showBannerHeading() {
+    bannerShade.style.top = 0;
+    bannerHeading.style.opacity = 1;
 }
 
 menuIcn.addEventListener('click', toggleNav);
--- projects/nichols/scripts/services.js
+++ projects/nichols/scripts/services.js
@@ -5,6 +5,7 @@ const bar1 = document.getElementById('bar_1');
 const bar2 = document.getElementById('bar_2');
 const bar3 = document.getElementById('bar_3');
 var menuShowing = false;
+var slideIndex = 1;
 
 function toggleNav() {
     if (!menuShowing) {
@@ -31,6 +32,41 @@ function hideMenu() {
     bar2.classList.remove('left_x');
     bar3.classList.remove('right_x');
 }
+
+function showBannerHeading() {
+    bannerShade.style.top = 0;
+    bannerHeading.style.opacity = 1;
+}
+
+function showSlides(n) {
+    var index;
+    var slides = document.getElementsByClassName('slides');
+    var dots = document.getElementsByClassName('dot');
+    if (n > slides.length) {
+        slideIndex = 1;
+    } 
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (index = 0; index < slides.length; index++) {
+        slides[index].style.display = 'none';
+    }
+    for (index = 0; index < dots.length; index++) {
+        dots[index].className = dots[index].className.replace(' active', '');
+    }
+    slides[slideIndex - 1].style.display = 'block';
+    dots[slideIndex - 1].className += ' active';
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+showSlides(slideIndex);
 
 menuIcn.addEventListener('click', toggleNav);
 navExit.addEventListener('click', hideMenu);
